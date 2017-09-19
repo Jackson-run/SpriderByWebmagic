c48bebf77c6ecb86858c9afa4815f707394a5acc
Jackson <1161609893@qq.com>
2017-09-20 00:13:44 +0800

    启动爬虫github

--- src/main/java/ustc/sse/SpriderByWebmagic/SpriderMain.java
+++ src/main/java/ustc/sse/SpriderByWebmagic/SpriderMain.java
@@ -9,7 +9,7 @@ public class SpriderMain extends Spider{
     }
 
     public static void main(String[] args) {
-        //启动爬虫
+        //启动爬虫测试github不同步
         create(new NewsPageProcessor()).addUrl("http://info.meadin.com/Index_1.shtml").thread(10).run();
     }
 }
